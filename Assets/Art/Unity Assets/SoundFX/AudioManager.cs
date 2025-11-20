ae5fd6c1ac650f8e20ffe2aec1c49d90dc56aa0f
1109-Robins-Aaron <ajrobins11@gmail.com>
2025-11-19 21:55:22 -0800

    Add a clicking sound effect and modified audioManager script

--- Assets/Art/Unity Assets/SoundFX/AudioManager.cs
+++ Assets/Art/Unity Assets/SoundFX/AudioManager.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
@@ -9,15 +11,45 @@ public class AudioManager : MonoBehaviour
     [Header("Audio Clips")]
     public AudioClip backgroundMusic;
     public AudioClip buttonCLickSound;
+    public AudioClip sliderChangeSound;
+    public AudioClip dropdownChangeSound;
+    [Header("Game Sounds")]
+    public AudioClip playerJumpSound;
+    public AudioClip playerAttacksound;
+    public AudioClip playerHurtSound;
+    public AudioClip enemyDeathSound;
+    public AudioClip enemyAttackSound;
+    public AudioClip enemyHurtSound;
+    public AudioClip itemPickupSound;
+
+    [Header("UI Components")]
+    public Slider slider;
+    public Dropdown dropdown;
 
     private void Start()
     {
         musicSource.clip = backgroundMusic;
         musicSource.Play();
+
+        if (slider != null)
+            slider.onValueChanged.AddListener(OnSliderValueChanged);
+        if (dropdown != null)
+            dropdown.onValueChanged.AddListener(OnDropdownValueChanged);
     }
     public void PlaySFX(AudioClip clip)
     {
+        if(clip !=null)
         sfxSource.PlayOneShot(clip);
+    }
+
+    public void OnSliderValueChanged(float value) 
+    {
+        PlaySFX(sliderChangeSound);
+    }
+
+    public void OnDropdownValueChanged(int index) 
+    {
+        PlaySFX(dropdownChangeSound);
     }
     public void PlayButtonClick()
     {
